3ed98b60300489befe76d6df6e128de5dc118eb5
alexli77 <38200963+alexli77@users.noreply.github.com>
2018-11-07 03:20:17 -0800

    Update and rename add.js to country.js

--- country.js
+++ country.js
@@ -14,3 +14,18 @@
             }
         });
     });
+
+    /*Display all countries*/
+    router.get('/filter/:country', function(req, res){
+        var callbackCount = 0;
+        var context = {};
+        var mysql = req.app.get('mysql');
+        getCountry(req,res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 2){
+                res.render('country', context);
+            }
+
+        }
+    });
