b96d0ff86b8bd77a3c2d3b60e189193cd97f43ee
alexli77 <38200963+alexli77@users.noreply.github.com>
2018-11-07 04:09:12 -0800

    Update country.js

--- country.js
+++ country.js
@@ -16,7 +16,7 @@
     });
 
     /*Display all countries*/
-    router.get('/filter/:country', function(req, res){
+    router.get('/', function(req, res){
         var callbackCount = 0;
         var context = {};
         var mysql = req.app.get('mysql');
@@ -29,3 +29,14 @@
 
         }
     });
+function getCountry(res, mysql, context, complete) {
+        mysql.pool.query("SELECT country_id AS cid, cname FROM endorse_country
+", function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.country = results;
+            complete();
+        });
+    }
